ed619223228558bf50e93cb33ff6d682dba94174
Martin Svoboda <svobma96@gmail.com>
2023-04-13 20:47:56 +0200

    Draw on hand move

--- hand-track/track.js
+++ hand-track/track.js
@@ -7,9 +7,9 @@ let model = null;
 
 const modelParams = {
   flipHorizontal: true, // flip e.g for video
-  maxNumBoxes: 1, // maximum number of boxes to detect
+  maxNumBoxes: 2, // maximum number of boxes to detect
   iouThreshold: 0.5, // ioU threshold for non-max suppression
-  scoreThreshold: 0.5, // confidence threshold for predictions.
+  scoreThreshold: 0.6, // confidence threshold for predictions.
 };
 
 function startVideo() {
@@ -24,11 +24,17 @@ function startVideo() {
 
 function runDetection() {
   model.detect(video).then((predictions) => {
-    const [prediction] = predictions;
+    const prediction = predictions.find((p) => p.label === 'open');
     if (prediction) {
-      console.log(prediction.bbox);
+      console.log(prediction.label);
+
+      context.fillStyle = 'black';
+      context.beginPath();
+      context.arc(prediction.bbox[0], prediction.bbox[1], 5, 0, 2 * Math.PI);
+      context.strokeStyle = '1px';
+      context.stroke();
     }
-    model.renderPredictions(predictions, canvas, context, video);
+    // model.renderPredictions(predictions, canvas, context, video);
     if (isVideo) {
       requestAnimationFrame(runDetection);
     }
